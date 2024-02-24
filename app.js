b68b2480095ebb1ea515ed6c5643ff70b3666654
mehranathon <mehran.khmn@gmail.com>
2024-02-24 09:15:05 -0800

    abstraction

--- app.js
+++ app.js
@@ -16,39 +16,43 @@ e.g.
 const spiral=(n)=>{
     const dim = Math.ceil(Math.sqrt(n))
     const centerline=Math.floor(dim/2)
+    let row,offSetL,offSetR,counter,currentSquare,terminus,currentVal,northAxis
     //sequence is a one-dimensional array with length n representing a square grid with dimensions dim x dim indexed increasing left to right
     const sequence=[]
-    const buildSequence=()=>{
-        let row=0
-        let offSetL=-1
-        let offSetR=dim+1
-        let counter=0
+    const initSpiral=()=>{
+        row=0
+        offSetL=-1
+        offSetR=dim+1
+        counter=0
         //currentSquare refers to root of perfect squares, which extend diagonally upward from the center: 3,5,7,... 
-        let currentSquare=dim
-        let terminus=Math.pow(currentSquare,2)+1
-        let currentVal
-        let northAxis=true
+        currentSquare=dim
+        terminus=Math.pow(currentSquare,2)+1
+        currentVal
+        northAxis=true
+    }
+
+    const iterateRow=()=>{
+        row++
+        counter=0
+        northAxis=row<=centerline
+        currentSquare=currentSquare+(2*(northAxis?-1:1))
+        terminus=northAxis?Math.pow(currentSquare,2)+1:sequence.at(-(dim-row))+1
+        offSetL+=(dim+(northAxis?1:-1))
+        offSetR+=(dim+(row===centerline+1?0:northAxis?-1:1))
+    }
+    const getVal=(i)=>{
+        if(i<=offSetL || i>=offSetR) return sequence.at(-dim)+(i%dim<centerline?-1:1);
+        if(northAxis) return terminus-currentSquare+counter
+        return terminus+currentSquare-1-counter
+    }
+    const buildSequence=()=>{
+        initSpiral()
         for(let i=0;i<n;i++){
             currentVal=null
-            if(i && i%dim===0){
-                row++
-                counter=0
-                northAxis=row<=centerline
-                currentSquare=currentSquare+(2*(northAxis?-1:1))
-                terminus=northAxis?Math.pow(currentSquare,2)+1:sequence.at(-(dim-row))+1
-                offSetL+=(dim+(northAxis?1:-1))
-                offSetR+=(dim+(row===centerline+1?0:northAxis?-1:1))
-            }
-            if(i<=offSetL || i>=offSetR){
-                currentVal=sequence.at(-dim)+(i%dim<centerline?-1:1)
-                sequence.push(currentVal)
-                continue
-            }
-            
-            if(northAxis) currentVal=terminus-currentSquare+counter
-            else currentVal=terminus+currentSquare-1-counter
-            sequence.push(currentVal)
-            counter++
+            if(i && i%dim===0)iterateRow()
+            const val=getVal(i)
+            if(i>offSetL && i<offSetR)counter++
+            sequence.push(val)
         }
     }
     buildSequence()
